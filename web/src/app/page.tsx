b605c2041e812a61eae9507cb921b500b10fb93c
Matheus Vidal <matheusvidalpro@gmail.com>
2023-05-20 02:24:34 -0400

    Finish configuring the web application.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>olá mundo</div>
+  return (
+    <div className="h-screen bg-zinc-900 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
+  )
 }
