ff64e7fdba137984a8d80e74d4873d75be48a952
Matheus Vidal <matheusvidalpro@gmail.com>
2023-05-20 01:30:44 -0400

    Configure ESLint to ensure code quality.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('🚀HTTP server running on http://localhost:3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀HTTP server running on http://localhost:3333')
+  })
